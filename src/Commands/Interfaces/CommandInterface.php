7d3faabd786ba7daa51e5f229e136e55e441a8ad
Melvin <melmups@outlook.com>
2020-07-28 12:08:40 +0200

    Documented code. Added support for adding meta information about commands

--- src/Commands/Interfaces/CommandInterface.php
+++ src/Commands/Interfaces/CommandInterface.php
@@ -6,5 +6,19 @@ namespace Merus\WAB\Commands\Interfaces;
 
 interface CommandInterface
 {
+    /**
+     * Execute a command
+     *
+     * @param CommandContextInterface $context The context of the execution
+     *
+     * @return mixed
+     */
     public function execute(CommandContextInterface $context);
+
+    /**
+     * Get meta information about the command
+     *
+     * @return array
+     */
+    public static function meta() : array;
 }
