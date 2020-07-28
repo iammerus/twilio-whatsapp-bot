ee17dac517bf06ba82573c04856f8f6942432660
Melvin <melmups@outlook.com>
2020-07-28 10:59:09 +0200

    Added config file

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'database' => [
+        //required
+        'username' => '',
+        'database' => '',
+        //optional
+        'password' => '',
+        'type' => 'mysql',
+        'charset' => 'utf8',
+        'host' => 'localhost',
+        'port' => '3309'
+    ]
+];
