4b5203dfebae4a57b6313e5919001c17fc095ca5
Melvin <melmups@outlook.com>
2020-07-28 12:25:18 +0200

    Imported constants file

--- public/index.php
+++ public/index.php
@@ -6,11 +6,13 @@ use Merus\WAB\Bot;
 define('ROOT_PATH', dirname(__DIR__));
 define('APP_ROOT', ROOT_PATH . '/src');
 
+require_once APP_ROOT . '/Helpers/constants.php';
+
 // Require in composer autoloader
 require_once ROOT_PATH . '/vendor/autoload.php';
 
 // Load configuration file
-$config = require_once APP_ROOT . '/config.php';
+$config = require_once APP_ROOT . '/Helpers/config.php';
 
 // Instantiate the bot handler app
 $bot = new Bot($config);
