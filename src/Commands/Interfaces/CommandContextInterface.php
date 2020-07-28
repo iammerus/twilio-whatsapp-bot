a1c861e24d17e2f7a13f8ba2824f3a805f1c39a7
Melvin <melmups@outlook.com>
2020-07-28 12:16:02 +0200

    Added the user's input to the context interface

--- src/Commands/Interfaces/CommandContextInterface.php
+++ src/Commands/Interfaces/CommandContextInterface.php
@@ -9,7 +9,24 @@ use Merus\WAB\ExecutionResult;
 
 interface CommandContextInterface
 {
+    /**
+     * Get the user's input text
+     *
+     * @return string
+     */
+    public function getUserInput(): string;
+
+    /**
+     * Get the app's database connection
+     *
+     * @return DB
+     */
     public function getDatabase(): DB;
 
+    /**
+     * Get the last execution result. Can be null
+     *
+     * @return ExecutionResult
+     */
     public function getExecutionResult(): ExecutionResult;
 }
