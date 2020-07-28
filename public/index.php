e6c1ff0d909948966c676762484d7999770f922c
Melvin <melmups@outlook.com>
2020-07-28 10:59:01 +0200

    Frontline public directory

--- public/index.php
+++ public/index.php
@@ -1,4 +1,19 @@
 <?php
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+use Merus\WAB\Bot;
 
+// Define path constants
+define('ROOT_PATH', dirname(__DIR__));
+define('APP_ROOT', ROOT_PATH . '/src');
+
+// Require in composer autoloader
+require_once ROOT_PATH . '/vendor/autoload.php';
+
+// Load configuration file
+$config = require_once APP_ROOT . '/config.php';
+
+// Instantiate the bot handler app
+$bot = new Bot($config);
+
+// Handle the incoming request
+$bot->handle();
