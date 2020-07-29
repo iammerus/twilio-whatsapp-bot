893b6f8aff8af5e1a1d1e4516a2646633fd52870
Melvin <melmups@outlook.com>
2020-07-29 08:50:54 +0200

    Updated context interface - added method to get the current request user from the context

--- src/Commands/Interfaces/CommandContextInterface.php
+++ src/Commands/Interfaces/CommandContextInterface.php
@@ -4,11 +4,20 @@
 namespace Merus\WAB\Commands\Interfaces;
 
 
+use Merus\WAB\Commands\User;
 use Merus\WAB\Database\DB;
 use Merus\WAB\ExecutionResult;
 
 interface CommandContextInterface
 {
+
+    /**
+     * Get the request's user
+     *
+     * @return User
+     */
+    public function getUser(): User;
+
     /**
      * Get the user's input text
      *
