ec7676d18cc86774185c8582806b3214189168c1
Melvin <melmups@outlook.com>
2020-07-29 11:55:38 +0200

    Updated create method to return an array

--- src/Commands/CommandOutput.php
+++ src/Commands/CommandOutput.php
@@ -8,50 +8,26 @@ use Merus\WAB\Commands\Interfaces\CommandContextInterface;
 
 class CommandOutput
 {
-    /**
-     * The command that was being executed
-     *
-     * @var string
-     */
-    private string $command;
-
-    /**
-     * The output of the command
-     *
-     * @var string
-     */
-    private string $output;
-
-    /**
-     *
-     * @var string
-     */
-    private string $status;
-    private string $meta;
-    private string $context;
-
-    private function __construct(string $command, string $output, string $status, string $meta, CommandContextInterface $context)
+    private function __construct()
     {
-        $this->command = $command;
-        $this->output = $output;
-        $this->status = $status;
-        $this->meta = $meta;
-        $this->context = $context;
     }
 
     /**
-     * Create an instance of the output class
+     * Create an output
      *
-     * @param string $command The command being executed
      * @param string $output The output of the command
      * @param string $status The status of the execution
-     * @param string $meta The meta information of the command
+     * @param array $meta The meta information of the command
      *
-     * @param Interfaces\CommandContextInterface $context
-     * @return CommandOutput
+     * @return array
      */
-    public static function create(string $command, string $output, string $status, string $meta, CommandContextInterface $context)
+    public static function create(string $output, string $status, array $meta)
     {
-        return new CommandOutput($command, $output, $status, $meta, $context);
+        send_message($output);
+
+        return [
+            'result' => $status,
+            'meta' => $meta
+        ];
     }
 }
