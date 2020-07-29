d2542af704c9ca0d942ff71efaf551abb994561c
Melvin <melmups@outlook.com>
2020-07-29 11:54:42 +0200

    Added method to pick a random response

--- src/Commands/CanSendMessages.php
+++ src/Commands/CanSendMessages.php
@@ -10,4 +10,29 @@ trait CanSendMessages
     {
         return send_message($body);
     }
+
+    public function randomElement(array &$array)
+    {
+        if(count($array) === 0) {
+            return null;
+        }
+
+        return $array[array_rand($array)];
+    }
+
+    /**
+     * Helper method for sending status update to registrar
+     *
+     * @param string $status The status of execution
+     * @param array $meta The meta information about the execution
+     *
+     * @return array
+     */
+    public function status(string $status, array $meta)
+    {
+        return [
+            'result' => $status,
+            'meta' => $meta
+        ];
+    }
 }
--- src/Commands/Defaults/UserInformationCommand.php
+++ src/Commands/Defaults/UserInformationCommand.php
@@ -4,22 +4,52 @@
 namespace Merus\WAB\Commands\Defaults;
 
 
+use Merus\WAB\Commands\CanSendMessages;
 use Merus\WAB\Commands\Interfaces\CommandContextInterface;
 use Merus\WAB\Commands\Interfaces\CommandInterface;
+use Merus\WAB\Commands\User;
 
 class UserInformationCommand implements CommandInterface
 {
+    use CanSendMessages;
 
     /**
      * @inheritDoc
      */
-    public function execute(CommandContextInterface $context)
+    public function execute(CommandContextInterface $context): array
     {
-        $db = $context->getDatabase();
-        $input = $context->getUserInput();
         $exec = $context->getExecutionResult();
 
-        $exec->last();
+        $user = $context->getUser();
+        $result = $exec->last($user);
+
+        if ($result && $result->command === 'default.information') {
+            $this->update($user, $context);
+
+            return $this->success($user->getName());
+        }
+
+        // Ask the user for their name
+        $this->ask();
+
+        // Return meta information for the registrar to save
+        return $this->status(COMMAND_EXECUTION_INCOMPLETE, []);
+    }
+
+    /**
+     * Ask the user for their name
+     */
+    protected function ask()
+    {
+        $responses = [
+            "Hi there! To get started, can you please tell me your name?",
+            "It looks like this is your first time texting me. Can you please tell me your name?",
+            "Hmm. I haven't seen you before. Could you please tell me your name?"
+        ];
+
+        $key = array_rand($responses);
+
+        $this->sendMessage($responses[$key]);
     }
 
     /**
@@ -40,5 +70,33 @@ class UserInformationCommand implements CommandInterface
             // Pattern to match or array of patterns to match
             'match' => null
         ];
+    }
+
+    protected function update(User $user, CommandContextInterface $context)
+    {
+        $user->setName($context->getUserInput());
+
+        $user->update($context->getDatabase());
+    }
+
+    /**
+     * When the command has been executed successfully to completion
+     *
+     * @param string|null $name
+     * @return array
+     */
+    protected function success(?string $name)
+    {
+        $responses = [
+            "Awesome! I'll call you {$name} from now on",
+            "Got it, {$name}. I'll remember your name",
+            "Okay, {$name}. I'll make sure to remember your name"
+        ];
+
+        $key = array_rand($responses);
+
+        $this->sendMessage($responses[$key] . ". To see what I can do, just send the message hello");
+
+        return $this->status(COMMAND_EXECUTION_COMPLETE, []);
     }
 }
