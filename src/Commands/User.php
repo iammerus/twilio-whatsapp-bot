a43e6aaa95ee8f9e3a6f159e1dd0ea87963ee98d
Melvin <melmups@outlook.com>
2020-07-29 08:51:07 +0200

    Added a model for the user

---
+++ src/Commands/User.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Merus\WAB\Commands;
+
+
+class User
+{
+    protected ?string $name;
+    protected string $number;
+
+    public function __construct(string $number, ?string $name)
+    {
+        $this->number = $number;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param string $number
+     */
+    public function setNumber(string $number): void
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+}
