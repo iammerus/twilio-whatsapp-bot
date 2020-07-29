5d39d28171b5160b398778d52b4fa85fdea44ff0
Melvin <melmups@outlook.com>
2020-07-29 08:49:53 +0200

    Scaffolded method for fetching the last execution result

--- src/ExecutionResult.php
+++ src/ExecutionResult.php
@@ -2,6 +2,7 @@
 
 namespace Merus\WAB;
 
+use Merus\WAB\Commands\User;
 use Merus\WAB\Database\DB;
 
 class ExecutionResult
@@ -24,8 +25,18 @@ class ExecutionResult
 
     }
 
-    public function last()
+    /**
+     * Fetch the last execution result for the given user
+     *
+     * @param User $user The user we're fetch exec result for
+     *
+     * @return object
+     */
+    public function last(User $user)
     {
-
+        return $this->db->row("SELECT * FROM `execution_results` 
+                                   WHERE `uid` = '{$user->getNumber()}' 
+                                   ORDER BY id DESC 
+                                   LIMIT 1");
     }
 }
